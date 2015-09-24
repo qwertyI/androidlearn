3681e483550404c51b12a5b92c3ee90d623d1fda
yuanning <727665171@qq.com>
2015-09-24 19:06:58 +0800

    会话消息界面的建立

--- Learn/app/src/main/java/com/example/qwerty/learn/ChatActivity.java
+++ Learn/app/src/main/java/com/example/qwerty/learn/ChatActivity.java
@@ -1,20 +1,112 @@
 package com.example.qwerty.learn;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChatActivity extends Activity {
+
+    private List<Message> messages = new ArrayList<Message>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_chat);
+        initMessage();
+        MessageAdapter adapter = new MessageAdapter(ChatActivity.this, R.layout.activity_chat_item, messages);
+        ListView listView = (ListView) findViewById(R.id.message_lv);
+        listView.setAdapter(adapter);
+    }
+
+    //消息适配器
+    public class MessageAdapter extends ArrayAdapter<Message>{
+        private int resourceId;
+
+        public MessageAdapter(Context context, int textViewResourceId, List<Message> objects){
+            super(context, textViewResourceId, objects);
+            resourceId = textViewResourceId;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup group){
+            Message message = getItem(position);
+            View view = convertView;
+            ViewHolder viewHolder;
+            if (view == null){
+                view = LayoutInflater.from(getContext()).inflate(resourceId,null);
+                viewHolder = new ViewHolder();
+                viewHolder.message_left_layout = (LinearLayout) findViewById(R.id.message_left_layout);
+                viewHolder.message_left_tv = (TextView) findViewById(R.id.message_left_tv);
+                viewHolder.message_right_layout = (LinearLayout) findViewById(R.id.message_right_layout);
+                viewHolder.message_right_tv = (TextView) findViewById(R.id.message_right_tv);
+                view.setTag(viewHolder);
+            }else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+            if (message.getMessageType() == Message.RECEIVE_MSG){
+                viewHolder.message_left_layout.setVisibility(View.VISIBLE);
+                viewHolder.message_right_layout.setVisibility(View.GONE);
+                viewHolder.message_left_tv.setText(message.getMessageBody());
+            }else if (message.getMessageType() == Message.SEND_MSG){
+                viewHolder.message_left_layout.setVisibility(View.GONE);
+                viewHolder.message_right_layout.setVisibility(View.VISIBLE);
+                viewHolder.message_right_tv.setText(message.getMessageBody());
+            }
+            return view;
+        }
+    }
+    //消息容器类
+    class ViewHolder{
+        LinearLayout message_left_layout;
+        LinearLayout message_right_layout;
+        TextView message_left_tv;
+        TextView message_right_tv;
+    }
+
+    //消息类
+    class Message{
+        private static final int RECEIVE_MSG = 0;
+        private static final int SEND_MSG = 1;
+
+        private String Message_Body;
+        private int Message_Type;
+
+        public Message(String message_Body, int type){
+            this.Message_Body = message_Body;
+            this.Message_Type = type;
+        }
+        public String getMessageBody(){
+            return Message_Body;
+        }
+        public int getMessageType(){
+            return Message_Type;
+        }
+    }
+
+    private void initMessage() {
+        Message msg1 = new Message("Hello guy.", Message.RECEIVE_MSG);
+        messages.add(msg1);
+        Message msg2 = new Message("Hello. Who is that?", Message.SEND_MSG);
+        messages.add(msg2);
+        Message msg3 = new Message("This is Tom. Nice talking to you. ", Message.RECEIVE_MSG);
+        messages.add(msg3);
     }
 
 
