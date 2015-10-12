b7ff254e22c5ec82822043161c75231ef25cd93a
yuanning <727665171@qq.com>
2015-10-12 11:02:03 +0800

    新增获取数据库数据，并且点击获取下一个数据的代码

--- Learn/app/src/main/java/com/example/qwerty/learn/DatabaseActivity.java
+++ Learn/app/src/main/java/com/example/qwerty/learn/DatabaseActivity.java
@@ -2,6 +2,7 @@ package com.example.qwerty.learn;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -12,16 +13,26 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class DatabaseActivity extends Activity implements OnClickListener{
 
+    private static final int CURSOR_NO_EXIST = 0;
     private static final int DATABASE_VERSION = 3;
     private static final String TABLE_BOOK = "book";
+    private static final String TABLE_BOOK_NAME = "name";
+    private static final String TABLE_BOOK_AUTHOR = "author";
+    private static final String TABLE_BOOK_PRICE = "price";
+    private static final String TABLE_BOOK_PAGE = "page";
 
     private MyDatabaseHelper dbHelper;
+    private Cursor cursor;
+    private SQLiteDatabase db;
+    private int Cursor_Id = 0;
 
     private Button insert_book_btn;
+    private Button show_data_btn;
     private EditText book_name_et;
     private EditText book_author_et;
     private EditText book_price_et;
@@ -38,12 +49,15 @@ public class DatabaseActivity extends Activity implements OnClickListener{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_database);
         dbHelper = new MyDatabaseHelper(this, TABLE_BOOK, null, DATABASE_VERSION);
+        db = dbHelper.getReadableDatabase();
         insert_book_btn = (Button) findViewById(R.id.insert_book_btn);
+        show_data_btn = (Button) findViewById(R.id.show_data_btn);
         book_name_et = (EditText) findViewById(R.id.book_name_et);
         book_author_et = (EditText) findViewById(R.id.book_author_et);
         book_price_et = (EditText) findViewById(R.id.book_price_et);
         book_page_et = (EditText) findViewById(R.id.book_page_et);
         insert_book_btn.setOnClickListener(this);
+        show_data_btn.setOnClickListener(this);
     }
 
     @Override
@@ -54,14 +68,55 @@ public class DatabaseActivity extends Activity implements OnClickListener{
                 author = book_author_et.getText().toString();
                 price = Double.parseDouble(book_price_et.getText().toString());
                 page = Integer.parseInt(book_page_et.getText().toString());
-                SQLiteDatabase db = dbHelper.getReadableDatabase();
+                Cursor_Id = Integer.parseInt(cursor.getString(cursor.getColumnIndex("id")));
+                cursor = null;
                 ContentValues values = new ContentValues();
-                values.put("name", name);
-                values.put("author", author);
-                values.put("price", price);
-                values.put("page", page);
-                db.insert("book", null, values);
+                Insert_Book(values, name, author, price, page);
+                Toast.makeText(this, "Insert Success", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.show_data_btn:
+                if (cursor == null){
+                    cursor = db.query("book",null,null,null,null,null,null);
+                }
+                if (Cursor_Id != CURSOR_NO_EXIST){
+                    cursor.moveToPosition(Cursor_Id - 1);
+                    Cursor_Id = CURSOR_NO_EXIST;
+                }
+                if (cursor.moveToNext()){
+                    name = cursor.getString(cursor.getColumnIndex(TABLE_BOOK_NAME));
+                    author = cursor.getString(cursor.getColumnIndex(TABLE_BOOK_AUTHOR));
+                    String sPrice = cursor.getString(cursor.getColumnIndex(TABLE_BOOK_PRICE));
+                    String sPage = cursor.getString(cursor.getColumnIndex(TABLE_BOOK_PAGE));
+                    Show_Book(name, author, sPrice, sPage);
+                }else {
+                    Show_Book("", "", "", "");
+                    Toast.makeText(this, "No Nextdata", Toast.LENGTH_SHORT).show();
+                    cursor.close();
+                    cursor = null;
+                }
+
         }
+    }
+
+
+
+
+
+    private void Show_Book(String name, String author, String  price, String page){
+        String sPrice = price+"";
+        String sPage = page + "";
+        book_name_et.setText(name);
+        book_author_et.setText(author);
+        book_price_et.setText(sPrice);
+        book_page_et.setText(sPage);
+    }
+
+    private void Insert_Book(ContentValues values, String name, String author, Double price, int page){
+        values.put("name", name);
+        values.put("author", author);
+        values.put("price", price);
+        values.put("page", page);
+        db.insert("book", null, values);
     }
 
 
